4eca776d33757a03029f0bf7a1b7bc9d76c09805
Felipe Rolon <afra.9622@gmail.com>
2025-02-15 00:54:50 -0500

    Lab 2 finished

--- main.cpp
+++ main.cpp
@@ -1,126 +1,178 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-struct Achievement
-{
-	std::string title;
-	std::string description;
-	float score = 0.0f;
+class Weapon {
+public:
+    std::string name;
+    int damage;
+    std::vector<std::string> specialAbilities;
+
+    Weapon() {
+        name = "Unknown Weapon";
+        damage = 0;
+        specialAbilities = {};
+    }
+
+    Weapon(std::string n, int d, std::vector<std::string> abilities) {
+        name = n;
+        damage = d;
+        specialAbilities = abilities;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Weapon& weapon) {
+        os << "Weapon: " << weapon.name << " | Damage: " << weapon.damage << " | Abilities: ";
+        for (int i = 0; i < 2; ++i) {
+            os << weapon.specialAbilities[i] << " and ";
+        }
+        return os;
+    }
+
+    bool operator==(const Weapon& other) const {
+        return name == other.name && damage == other.damage;
+    }
 };
 
-struct Game
-{
-	std::string name;
-	std::string publisher;
-	std::string developers;
-	Achievement* achievements = nullptr;
-	int achievementCount = 0;
+class Armour {
+public:
+    std::string name;
+    std::string description;
+    int armourValue;
+
+    Armour() {
+        name = "Unknown armour";
+        description = "desc";
+        armourValue = 0;
+    }
+
+    Armour(std::string n, std::string desc, int av) {
+        name = n;
+        description = desc;
+        armourValue = av;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Armour& armour) {
+        os << "Armour: " << armour.name << " | Defence: " << armour.armourValue << " | Description: " << armour.description;
+        return os;
+    }
+
+    bool operator==(const Armour& other) const {
+        return name == other.name && armourValue == other.armourValue;
+    }
 };
 
-struct Platform
-{
-	std::string name;
-	std::string manufacturer;
-	Game* games = nullptr;
-	int gameCount = 0;
+class Character {
+public:
+    std::string name;
+    std::string species;
+    std::string backstory;
+    int health;
+    Weapon weapon;
+    Armour armour;
+
+    Character() {
+        name = "Unknown character";
+        species = "None";
+        backstory = "None";
+        health = 50;
+        weapon = Weapon();
+        armour = Armour();
+    }
+
+    Character(std::string n, std::string s, std::string b, int h, Weapon w, Armour a) {
+        name = n;
+        species = s;
+        backstory = b;
+        health = h;
+        weapon = w;
+        armour = a;
+    }
+
+    void displayInfo() const {
+        std::cout << "\nCharacter Name: " << name
+            << "\nSpecies: " << species
+            << "\nBackstory: " << backstory
+            << "\nHealth: " << health << std::endl;
+        std::cout << weapon << std::endl;
+        std::cout << armour << "\n" << std::endl;
+    }
+
+    bool operator==(const Character& other) const {
+        if (name == other.name && species == other.species && health == other.health && weapon == other.weapon && armour == other.armour) {
+            std::cout << "\nCharacters are the same!\n";
+            return true;
+        }
+        else {
+            std::cout << "\nCharacters are NOT the same!\n";
+            return false;
+        }
+    }
 };
 
-// -Stores platforms
-// -Each player can have multiple games
-// -Each game can have multiple achievements
+int main() {
+    std::cout << "Welcome! customize three characters\n";
 
-int main()
-{
-	int platformCount;
-	printf("Enter the number of platforms.\n");
-	std::cin >> platformCount;
-	
+    Character characters[3];
+    Weapon weapons[3] = {
+        {"Longsword", 50, {"Heavy Swing", "Precision Thrust"}},
+        {"War Axe", 55, {"Cleave", "Shield Breaker"}},
+        {"Crossbow", 40, {"Piercing Bolt", "Quick Reload"}}
+    };
 
-	Platform* platforms = new Platform[platformCount];
-	printf("There will be %i platforms.\n", platformCount);
+    Armour armours[3] = {
+        {"Chainmail", "Balanced protection with mobility", 35},
+        {"Plate Armour", "Heavy but strong defense", 50},
+        {"Gambeson", "Light padding for agility", 20}
+    };
 
-	for (int i = 0; i < platformCount; i++)
-	{
-		Platform& platform = platforms[i];
+    for (int i = 0; i < 3; ++i) {
+        std::string name, species, backstory;
+        int health, weaponChoice, armourChoice;
 
-		int platformNumber = i + 1;
-		printf("Please input the name of platform %i \n", platformNumber);
-		std::cin >> platform.name;
-		
+        std::cout << "\nCreating Character " << (i + 1) << ":" << std::endl;
+        std::cout << "Enter character name: ";
+        std::cin.ignore();
+        std::getline(std::cin, name);
+        std::cout << "Enter species: ";
+        std::getline(std::cin, species);
+        std::cout << "Enter backstory: ";
+        std::getline(std::cin, backstory);
+        std::cout << "Enter starting health (50-200): ";
+        std::cin >> health;
+        health = std::max(50, std::min(200, health));
 
-		printf("What is the manufacturer of platform %i?\n", platformNumber);
-		std::cin >> platform.manufacturer;
-		
+        std::cout << "Choose a weapon:\n";
+        for (int j = 0; j < 3; ++j) {
+            std::cout << j + 1 << ". " << weapons[j].name << " (Damage: " << weapons[j].damage << ")\n";
+        }
+        std::cin >> weaponChoice;
+        weaponChoice = std::max(1, std::min(3, weaponChoice)) - 1;
 
-		printf("Please input the number of games platform %i have (Min 5)\n", platformNumber);
-		std::cin >> platform.gameCount;
-		
-		if (platform.gameCount < 5) platform.gameCount = 5;
+        std::cout << "Choose an armour:\n";
+        for (int j = 0; j < 3; ++j) {
+            std::cout << j + 1 << ". " << armours[j].name << " (Defence: " << armours[j].armourValue << ")\n";
+        }
+        std::cin >> armourChoice;
+        armourChoice = std::max(1, std::min(3, armourChoice)) - 1;
 
+        characters[i] = Character(name, species, backstory, health, weapons[weaponChoice], armours[armourChoice]);
+        characters[i].displayInfo();
+    }
 
-		platform.games = new Game[platform.gameCount];
-		//printf("The platform %i have %i games.\n", platformNumber, platform.gameCount);
+    std::cout << "\nCompare two characters by index (1-3): ";
+    int index1, index2;
+    std::cin >> index1 >> index2;
+    if (index1 != index2 && index1 >= 1 && index1 <= 3 && index2 >= 1 && index2 <= 3) {
+        characters[index1 - 1] == characters[index2 - 1];
+    }
+    else {
+        std::cout << "Invalid comparison indices.\n";
+    }
 
-		for (int j = 0; j < platform.gameCount; j++)
-		{
-			Game& game = platform.games[j];
-			int gameNumber = j + 1;
+    std::cout << "\nDisplaying all characters:\n";
+    for (int i = 0; i < 3; ++i) {
+        characters[i].displayInfo();
+    }
 
-			printf("Please input the name of game %i\n", gameNumber);
-			std::cin >> game.name;
-			
-
-			printf("Please input the publisher of game %i\n", gameNumber);
-			std::cin >> game.publisher;
-			
-
-			printf("Who are the developers of the game %i?\n", gameNumber);
-			std::cin >> game.developers;
-			
-
-			printf("Please input the number of achievements game %i have (Min 5)\n", gameNumber);
-			std::cin >> game.achievementCount;
-			
-			if (game.achievementCount < 5) game.achievementCount = 5;
-
-			game.achievements = new Achievement[game.achievementCount];
-			//printf("Game %i has %i achievements.\n", gameNumber, game.achievementCount);
-
-			for (int k = 0; k < game.achievementCount; k++)
-			{
-				Achievement& achievement = game.achievements[k];
-				int achievementNumber = k + 1;
-
-				printf("Please input the title of achievement %i\n", achievementNumber);
-				std::cin >> achievement.title;
-				
-
-				printf("Please input the description of achievement %i\n", achievementNumber);
-				std::cin >> achievement.description;
-				
-
-				printf("Please input a decimal for the score value of achievement %i?\n", achievementNumber);
-				std::cin >> achievement.score;
-				
-			}
-		}
-	}
-	//Print everything to the console and delete
-	for (int i = 0; i < platformCount; i++) {
-		printf("Platform %i has the name: %s ,the manufacturer: %s and the games:\n", i + 1, platforms[i].name.c_str(), platforms[i].manufacturer.c_str());
-		for (int j = 0; j < platforms[i].gameCount; j++) {
-			printf("	Game %i has the name: %s, the publisher: %s, the developers %s and the achievements:\n", j + 1, platforms[i].games[j].name.c_str(), platforms[i].games[j].publisher.c_str(), platforms[i].games[j].developers.c_str());
-			for (int k = 0; k < platforms[i].games[j].achievementCount; k++) {
-				printf("		Achievement %i has the title: %s, the description: %s and the score value of: %f\n", k + 1, platforms[i].games[j].achievements[k].title.c_str(), platforms[i].games[j].achievements[k].description.c_str(), platforms[i].games[j].achievements[k].score);
-			}
-			delete[] platforms[i].games[j].achievements;
-		}
-		delete[] platforms[i].games;
-	}
-	delete[] platforms;
-
-	printf("Press any key to finish");
-	std::cin >> platformCount;	
-
-	return 0;
+    return 0;
 }
